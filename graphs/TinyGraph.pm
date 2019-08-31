bb82dd78bff08fc87e7309ce79dd93db765ae510
Richard Loveland <rloveland@terrys-air.home>
2019-08-30 23:11:43 -0400

    Add `get_attributes()` method

--- graphs/TinyGraph.pm
+++ graphs/TinyGraph.pm
@@ -423,6 +423,12 @@ sub _st_add {
     $st->{$neighbor}->{prev} = $vertex;
 }
 
+sub get_attributes {
+    my ( $self, $start, $end ) = @_;
+    my $pairkey = $start . $end;
+    return $attrs->{$pairkey};
+}
+
 sub get_attribute {
     my ( $self, $start, $end, $attribute ) = @_;
 
